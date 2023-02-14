8a2bc2bc199995f863c7279b60e1975632fcf3cb
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-02-14 19:59:12 +0000

    added new comments

--- controller/dialogFlow.js
+++ controller/dialogFlow.js
@@ -10,7 +10,7 @@ const sessionId = uuid.v4();
 async function runIntent(projectId, requestText,event) {
 // A unique identifier for the given session
 console.log("hello")
-// console.log(projectId,requestText,event,process.env.DIALOGFLOW_PRIVATE_KEY,process.env.DIALOGFLOW_CLIENT_EMAIL)
+console.log(projectId,requestText,event,process.env.DIALOGFLOW_PRIVATE_KEY,process.env.DIALOGFLOW_CLIENT_EMAIL)
 try{
 
     let config = {
@@ -60,8 +60,9 @@ const intentRequestIntro = {
 // // // Send request and log result
 
 if(event){
-    console.log("event is not there::::: ")
+    console.log("event is there::::: ")
     const responses = await sessionClient.detectIntent(intentRequestIntro);
+    console.log("after event::::: ")
 console.log(responses[0].queryResult.fulfillmentMessages[0].payload)
 const result = responses[0].queryResult;
 console.log(responses[0].responseId)
@@ -74,7 +75,7 @@ return await {
 };
 }
 else{
-    console.log("before detect there::::: ")
+    console.log(" event not there before detect there::::: ")
     const responses = await sessionClient.detectIntent(intentRequest);
     console.log("after detec ")  
     console.log(responses[0].queryResult)
--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,23 @@ router.post("/requestText", function(req, res){
         console.log("request body :: ",req.body)
         var result = await runIntent(process.env.DIALOGFLOW_PROJECT_ID, req.body.requestText,req.body.name);
         console.log("the result",result)
-  //       return res.send(
-  //         {
-  //             "response":"active"
-  //         });
+        return res.send(
+          {
+              "response":"active"
+          });
       
-  // })()
+  })()
         // var result = await runIntent('test2-tsgl', 'hi')
        
-        return res.send(
-            {
-                "id":result.id,
-                "responseMessage": result.Response,
-                "originalQuery": result.Query,
-                "intent": result.Intent
-            });
+    //     return res.send(
+    //         {
+    //             "id":result.id,
+    //             "responseMessage": result.Response,
+    //             "originalQuery": result.Query,
+    //             "intent": result.Intent
+    //         });
         
-    })()
+    // })()
 
 });
 //'https://opentdb.com/api.php?amount=10'
