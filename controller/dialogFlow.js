c670a2202d15c3646a320d843741ca23731b4382
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-02-14 20:04:45 +0000

    revamped

--- controller/dialogFlow.js
+++ controller/dialogFlow.js
@@ -10,7 +10,7 @@ const sessionId = uuid.v4();
 async function runIntent(projectId, requestText,event) {
 // A unique identifier for the given session
 console.log("hello")
-console.log(projectId,requestText,event,process.env.DIALOGFLOW_PRIVATE_KEY,process.env.DIALOGFLOW_CLIENT_EMAIL)
+// console.log(projectId,requestText,event,process.env.DIALOGFLOW_PRIVATE_KEY,process.env.DIALOGFLOW_CLIENT_EMAIL)
 try{
 
     let config = {
@@ -56,13 +56,11 @@ const intentRequestIntro = {
     },
 };
 
-// // // The text query request.
-// // // Send request and log result
+// // The text query request.
+// // Send request and log result
 
 if(event){
-    console.log("event is there::::: ")
     const responses = await sessionClient.detectIntent(intentRequestIntro);
-    console.log("after event::::: ")
 console.log(responses[0].queryResult.fulfillmentMessages[0].payload)
 const result = responses[0].queryResult;
 console.log(responses[0].responseId)
@@ -75,9 +73,8 @@ return await {
 };
 }
 else{
-    console.log(" event not there before detect there::::: ")
     const responses = await sessionClient.detectIntent(intentRequest);
-    console.log("after detec ")  
+    
     console.log(responses[0].queryResult)
     const result = responses[0].queryResult;
     console.log(responses[0].responseId)
