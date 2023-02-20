7e5e935ec9e9861d5421861c3e6b5b9ce3c04383
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-02-20 14:42:00 +0000

    commented credentials

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const ssmClient = new SSMClient({
-  region: "us-east-1",
-  credentials: {
-    accessKeyId: "ASIARFTZFSZJVMDKYQMC",
-    secretAccessKey: "nnpcOyLXzK2flPtcvvYhKuFXlDOCHsgp8AIZfRa3",
-    sessionToken: "FwoGZXIvYXdzEB0aDP7GyC9U0LSEMRnyKyLCAexCn2J5D1LRhALviWzag7ZnwQE1Sgd8WpL7l+inO8h1NqxhO7tWOECa0h+0jLnIIKTSqCs4uciHfNyYQ8oSVL4ASqOETueYDRVQmZ7M5zBQPFESolWu4AAVEBoUHrxmWHW4+keFRrMJG0dzLkZHkHUqvRLTs7QISv2PrSfpeO7Rk/t4r09EbV5DqzStAwsoGYOd+wfT//BLii3j+tJm6cfpQDzonIucIMIZSbPE+/Ov9l1vaErE6sZsVitj2ToNLj3hKNKpzZ8GMi0VN0OAHAdrUi+PQEYSBRgA8kTiEgpWSh0h14P8UVrwduMqR9dK7aBNZNIGoBA="
-  },
-});
+// const ssmClient = new SSMClient({
+//   region: "us-east-1",
+//   credentials: {
+//     accessKeyId: "ASIARFTZFSZJVMDKYQMC",
+//     secretAccessKey: "nnpcOyLXzK2flPtcvvYhKuFXlDOCHsgp8AIZfRa3",
+//     sessionToken: "FwoGZXIvYXdzEB0aDP7GyC9U0LSEMRnyKyLCAexCn2J5D1LRhALviWzag7ZnwQE1Sgd8WpL7l+inO8h1NqxhO7tWOECa0h+0jLnIIKTSqCs4uciHfNyYQ8oSVL4ASqOETueYDRVQmZ7M5zBQPFESolWu4AAVEBoUHrxmWHW4+keFRrMJG0dzLkZHkHUqvRLTs7QISv2PrSfpeO7Rk/t4r09EbV5DqzStAwsoGYOd+wfT//BLii3j+tJm6cfpQDzonIucIMIZSbPE+/Ov9l1vaErE6sZsVitj2ToNLj3hKNKpzZ8GMi0VN0OAHAdrUi+PQEYSBRgA8kTiEgpWSh0h14P8UVrwduMqR9dK7aBNZNIGoBA="
+//   },
+// });
 module.exports = {
   
     someFunction: async function() {
