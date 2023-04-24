aa9d1486dbd3b45e8cce9c6b6277039d37fe1d9a
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-24 20:04:35 +0100

    u

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ async function someFunction(){
           const command3 = new GetParameterCommand(privatekey);
          const  key = await ssmClient.send(command3);
         var  Dkey= key.Parameter.Value;
-        console.log("got command",Demail,Dkey,id)
+   
       
         return [Dkey,Demail,id];
 }
