de84aba1210d9d78efb67dda66ab79a8eefd56c9
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-24 11:12:15 +0100

    g6

--- routes/index.js
+++ routes/index.js
@@ -392,6 +392,7 @@ const presentUser = await client.send(new GetItemCommand(params))
     }
   });
   router.get('/user', async (req, res) => {
+    const { user } = await connectDb();
     const paramsCheck = {
       TableName: user,
       Key: {
