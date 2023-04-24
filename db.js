3e373c1d0fe777e6a645a76cc72439d825d53089
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-24 13:34:24 +0100

    ft

--- db.js
+++ db.js
@@ -4,13 +4,13 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 
 let client;
 const quiz = 'quiz'
-const user = 'user'
+const users = 'users'
 const prequiz ='prequiz'
 const postquiz='postquiz'
 async function connectDb() {
   if (client) {
     console.log('Using existing client instance harsha');
-    return{ client, quiz,user,prequiz,postquiz };
+    return{ client, quiz,users,prequiz,postquiz };
   } else {
     //new connection
     client = new DynamoDBClient({
@@ -25,9 +25,9 @@ async function connectDb() {
     //     sessionToken: "FwoGZXIvYXdzEKH//////////wEaDDgmFrpVkLQKfh16CiLCAeMCc4JnFnpV7giCqRFqz17Y7gexwpIWQJ4wUXlyJxWn3PEvhxC47fd/rZM/I4OgbT1WnmcGeHggcTa75cSsnxABdW8bmLOoOIK6PN4vnM0kIXLefpcny6XgLmICd5sDjp4+/RaQnGaluo2IRglQw3B6NEY7PCGDxm7ZFXRsIWvJYPgAEV22zb0J2FrlO82JInHEjgBoYPgVBOuVPjzAp2Hn9Okd+GdEFXiTQNCBLCn1njdRu4Nmukrcp51pBZ9WhVNyKK24y6EGMi0ypQPsWNIfDbMw049qQg86t+7//45wvNElwq+XP9Y+r7BJG6YJZzYpThyoRec="
     //   }
     // });
- console.log("client  ",client)
-    console.log('Created new client instance harsha',quiz);
-    return { client, quiz,user,prequiz,postquiz };
+
+
+    return { client, quiz,users,prequiz,postquiz };
   }
 }
 
